26b91cae495a1e22bf479a6122fbe1c6cb8d5b16
Pooja <pooja.salot@accionlabs.com>
2019-04-18 11:56:03 +0530

    event listening initiated

--- kafka_avro_producer.js
+++ kafka_avro_producer.js
@@ -1,18 +1,18 @@
 const KafkaAvro = require("kafka-avro");
 const config = require("./config");
 const kafkaAvro = new KafkaAvro(config.avroConfigurations);
-const bunyanLog = require("bunyan-format");
 
-const kafkaLog = KafkaAvro.getLogger();
-
-kafkaLog.addStream({
-	type: "stream",
-	stream: bunyanLog({
-		outputMode: "short",
-		levelInString: true
-	}),
-	level: config.avroLogLevel
-});
+//uncomment line nos 6 to 15 if you want to print logs
+// const bunyanLog = require("bunyan-format");
+// const kafkaLog = KafkaAvro.getLogger();
+// kafkaLog.addStream({
+// 	type: "stream",
+// 	stream: bunyanLog({
+// 		outputMode: "short",
+// 		levelInString: true
+// 	}),
+// 	level: config.avroLogLevel
+// });
 
 // console.log(KafkaAvro.CODES);
 
@@ -28,7 +28,6 @@ function initKafkaAvro(kafkaConfig) {
 				return kafkaAvro
 					.getProducer(kafkaConfig)
 					.then(function(producerData) {
-						console.log("producer", producerData);
 						producer = producerData;
 						producerData.on("delivery-report", function(err, report) {
 							console.log("delivery-report: " + JSON.stringify(report), err);
@@ -36,7 +35,7 @@ function initKafkaAvro(kafkaConfig) {
 						});
 						producerData.on("producer error", err => {
 							console.log(err, "producer error");
-							process.exit(1);
+							// process.exit(1);
 						});
 						producer.on("event.log", function(log) {
 							console.log("producer log:", log);
@@ -59,66 +58,35 @@ function initKafkaAvro(kafkaConfig) {
 			});
 	});
 }
-
-// console.log(producer, "outer producer");
 function produceMessage(producer, topicName, partition = -1, message = {}) {
 	try {
-		console.log("Sending data to kafka topic");
-
 		// const type = kafkaAvro.sr.valueSchemas[topicName];
 		// const schemaId = kafkaAvro.sr.schemaMeta[topicName].id;
-		// console.log(type, schemaId);
-		//kafkaAvro.serialize("record",2, avrotest);
-		// const key = topicName + new Date();
-		let key = { id: Math.random().toString() };
-		let value = { id: Math.random().toString(), name: "Thanos" };
 		// kafkaAvro.serialize(type, schemaId, messageData);
 
-		// if partition is set to -1, librdkafka will use the default partitioner
-		// const partition = -1;
-		// console.log(topicName);
+		let key = { id: Math.random().toString() };
+
 		setInterval(function() {
 			console.log("produce message");
-			producer.produce(topicName, partition, value, key, Date.now());
+			producer.produce(topicName, partition, message, key, Date.now());
 		}, 10000);
-		// console.log("jsf");
 	} catch (e) {
 		console.log(e);
 	}
 }
-let counter = 0;
-// setInterval(async function() {
-// 	// console.log(producer, "int");
+
 const messageData = { id: Math.random().toString(), name: "Test" };
-// 	console.log(counter++);
-// 	await produceMessage(producer, config.topicName, -1, messageData);
-// }, 10000);
+
 try {
 	initKafkaAvro(config.kafkaConfig)
 		.then(producer => {
-			// console.log(producer);
-			producer.on("disconnected", function(arg) {
-				console.log("producer disconnected. " + JSON.stringify(arg));
-			});
-			producer.on("error", function(err) {
-				console.log("error from producer", err);
-			});
-			producer.on("event.log", function(log) {
-				console.log("eventlog", log);
-			});
-			producer.on("event.error", function(err) {
-				console.log("Error from producer in event error");
-				console.log(err);
-			});
-
-			producer.on("ready", function(err, success) {
-				console.log("in ready");
-			});
+			setInterval(() => {
+				producer.poll();
+			}, config.kafkaPollInterval);
 			produceMessage(producer, config.topicName, -1, messageData);
 		})
 		.catch(err => {
 			console.log(err, "error outer");
-			// throw new Error(err);
 			initErrorHandler(err);
 		});
 } catch (e) {
@@ -127,30 +95,4 @@ try {
 
 function initErrorHandler(err) {
 	console.log("in init");
-	//can add custom logic to handle errors
 }
-// const t = setInterval(function() {
-// 	if (producer) {
-// 		clearInterval(t);
-// 		producer.on("error", function(err) {
-// 			console.log("Error from producer:", err);
-// 		});
-// 		producer.on("disconnected", function(arg) {
-// 			console.log("producer disconnected. " + JSON.stringify(arg));
-// 		});
-// 		producer.on("error", function(err) {
-// 			console.log("error from producer", err);
-// 		});
-// 		producer.on("event.log", function(log) {
-// 			console.log(log);
-// 		});
-// 		producer.on("event.error", function(err) {
-// 			console.log("Error from producer");
-// 			console.log(err);
-// 		});
-// 		producer.on("delivery-report", function(err, report) {
-// 			console.log("delivery-report: " + JSON.stringify(report), err);
-// 			// counter++;
-// 		});
-// 	}
-// }, 1000);
